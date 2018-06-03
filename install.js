d471db31f19b973d2f20cfa0c4075896226ae02c
1024x2 <cod1nggam3rhd@gmail.com>
2018-06-03 14:36:20 +0100

    you missed a spot

--- install.js
+++ install.js
@@ -3,7 +3,7 @@ var _main = `var interval = setInterval(function () {
     if (!!le) {
       localStorage[".pkg93/userscript"] = "";
       clearInterval(interval);
-      $loader.script("https://rawgit.com/pkg93/pkg93/master/pkg93.js";
+      $loader.script("https://rawgit.com/pkg93/pkg93/master/pkg93.js");
     }
   } catch (err) {
     console.error(err);
